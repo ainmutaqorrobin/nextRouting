a82ab266b81b7ab8594c34901887fb6507c408ce
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-07-29 07:14:59 +0800

    optional catch all segments route

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,52 @@
+import NewsList from "@/components/news-list";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/util/news";
+import Link from "next/link";
+
+export default function ArchiveYearPage({ params: { filter } }) {
+  console.log(filter);
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let newsContent = <p>No news found for selected date.</p>;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  if (news && news.length > 0) newsContent = <NewsList news={news} />;
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}
--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/util/news";
-
-export default function ArchiveYearPage({ params: { year } }) {
-  const news = getNewsForYear(year);
-
-  return <NewsList news={news} />;
-}
