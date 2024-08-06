636e739b65f9beb8b7657f1d9a2e3987f69ac7ba
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-08-06 12:14:58 +0800

    server side fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,37 +1,15 @@
-"use client";
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [news, SetNews] = useState();
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+  if (!response) throw new Error("Failed to fetch news.");
 
-      if (!response.ok) {
-        setError("Failed to fetch news.");
-        setIsLoading(false);
-      }
-      const news = await response.json();
-      setIsLoading(false);
-      SetNews(news);
-    }
+  const news = await response.json();
 
-    fetchNews();
-  }, []);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
-
-  let newsContent;
-  if (news) newsContent = <NewsList news={news} />;
   return (
     <>
       <h1>This is news page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
